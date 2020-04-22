6a27f8580f66ad82c00a5d75c96f97abf3e20b86
Blayze <blayzestone@protonmail.com>
2020-04-22 15:26:35 -0700

    style: style Form component with styled-components

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,39 +1,95 @@
 import React from 'react';
+import styled from 'styled-components';
+
+/****** STYLED COMPONENTS ******/
+const FormContainer = styled.div`
+  width: 50vw;
+  margin: 10vh auto 0;
+`;
+
+const StyledForm = styled.form`
+  height: 33vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  background-color: #F5F5F5;
+  padding: 1rem;
+`;
+
+const Label = styled.label`
+  width: 80%;
+`;
+
+const Input = styled.input`
+  padding: .2rem;
+  margin: .5rem 0;
+  width: 100%;
+`;
+
+const ErrorMessage = styled.span`
+  font-size: 12px;
+  color: orangered;
+`;
+
+const Button = styled.button`
+  width: 50%;
+  color: white;
+  background-color: #60EE60;
+  border: none;
+  border-radius: 3px;
+  padding: 0.5rem 0;
+  margin-top: 2rem;
+  
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
 
 function Form(props) {
   const { onInputChange, onSubmit, errors } = props;
   return (
-    <form>
-      <label htmlFor="name">Name:</label>
-      <input
-        onChange={onInputChange} 
-        type="text" 
-        name="name"
-        id="name"
-      /><span>{errors.name}</span>
-      <label htmlFor="email">Email:</label>
-      <input 
-        onChange={onInputChange} 
-        type="text"
-        name="email"
-        id="email"
-      /><span>{errors.email}</span>
-      <label htmlFor="password">Password:</label>
-      <input 
-        onChange={onInputChange} 
-        type="text"
-        name="password"
-        id="password"
-      /><span>{errors.password}</span>
-      <label htmlFor="tos">Terms of Service:</label>
-      <input
-        onChange={onInputChange} 
-        type="checkbox" 
-        name="tos"
-        id="tos"
-      />
-      <button onClick={onSubmit}>Submit</button>
-    </form>
+    <FormContainer>
+      <StyledForm>
+        <ErrorMessage>{errors.name}</ErrorMessage>
+        <Label>Name:
+          <Input
+            onChange={onInputChange} 
+            type="text" 
+            name="name"
+          />
+        </Label>
+
+        <ErrorMessage>{errors.email}</ErrorMessage>
+        <Label>Email:
+          <Input 
+            onChange={onInputChange} 
+            type="text"
+            name="email"
+          />
+        </Label>
+
+        <ErrorMessage>{errors.password}</ErrorMessage>
+        <Label>Password:
+          <Input 
+            onChange={onInputChange} 
+            type="text"
+            name="password"
+            id="password"
+          />
+        </Label>
+
+        <Label>Terms of Service:
+          <input
+            onChange={onInputChange} 
+            type="checkbox" 
+            name="tos"
+            id="tos"
+          />
+        </Label>
+        <Button onClick={onSubmit}>Submit</Button>
+      </StyledForm>
+    </FormContainer>
   );
 }
 
